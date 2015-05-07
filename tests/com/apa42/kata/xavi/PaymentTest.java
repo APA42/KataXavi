7ebed4bf0c2935e91f00b4760c80e1dd9bc59f65
APA42 <APA42@MacBook-Pro-de-Alberto.local>
2015-05-07 19:49:27 +0200

    Red...

--- tests/com/apa42/kata/xavi/PaymentTest.java
+++ tests/com/apa42/kata/xavi/PaymentTest.java
@@ -1,7 +1,11 @@
 package com.apa42.kata.xavi;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.util.List;
 
@@ -12,6 +16,14 @@ public class PaymentTest {
     //1 - If the payment is for a physical product, generate a packing slip for shipping.
     private Payment myPayment;
     private PhysicalProduct aPhysicalProduct;
+
+    @Spy
+    private Sender aSender;
+
+    @Before
+    public void before(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void whenPhysicalProductThenGeneratePackingSlip() {
@@ -35,4 +47,13 @@ public class PaymentTest {
         Assert.assertEquals(result.get(0).data(), result.get(1).data());
     }
 
+    @Test
+    public void whenBookProductThenCreateDuplicatePackingSlipAndSendToRoyaltyDepartment() {
+        BookProduct book = new BookProduct();
+
+        myPayment = new Payment(aSender);
+
+        List<ShippingSlip> result = myPayment.payForBookProduct(book);
+
+        Assert.assertEquals(result.get(0).data(), result.get(1).data());
 }
